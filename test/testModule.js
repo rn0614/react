0ffd1e4c8fe695164a867ecc4223ea0c9d153ea0
koo <rn0614@naver.com>
2022-09-30 22:18:47 +0900

    ww

--- test/testModule.js
+++ test/testModule.js
@@ -21,3 +21,16 @@ function test2(name){
 }
 
 export {test1, test2}; // 이후에 export로 한번에 선언도 가능
+
+
+// default 사용시 해당 모듈의 객체는 하나로 지정됨
+// default 는 import  시 import User from './testModule.js'; 같이 {} 없이 사용 가능
+//import 시 이름을 마음대로 설정 가능
+export default class User {
+    constructor(name){
+        this.name =name
+    }
+}
+
+// default는 이름 없어도 됨
+// export default class {...} 식으로 사용 가능   
