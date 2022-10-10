a17c7f7cb57f0ee1b4361a8b26c68c2e3f18b772
koo <rn0614@naver.com>
2022-10-10 23:56:25 +0900

    ww

---
+++ test/test14.js
@@ -0,0 +1,19 @@
+//1.js의 값중 특이값
+//  infinity, NaN, undefined, null, globalThis(전역 this값)
+
+// js는 최대값이 존재
+const maxNum = Math.pow(10,1000);
+
+if(maxNum===Infinity){
+    console.log(`maxNum은 Infinity 입니다.`)
+}
+
+
+//2. 함수속성
+// eval, isFinite(), isNaN()
+// parseFloat(), parseInt(), 
+// encodeURI(), decodeURI(), encodeURIComponent(), decodeURIComponent()
+
+
+//3. 기초 객체의 종류
+// Object, Function, Boolean, Symbol 
