cf110d6501e70a7142c2788e9e30be6a2afa289b
koo <rn0614@naver.com>
2022-11-08 23:51:36 +0900

    react200 start1

---
+++ react/react200/client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ImportComponent from './R003_ImportComponent';
+
+// react 첫 페이지 적용하는 곳
+function App() {
+  return (
+    <div className="App">
+      <h1>Start React</h1>
+      <p>HTML적용단</p>
+      <ImportComponent></ImportComponent>
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react200/client/src/R003_ImportComponent.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+
+// render() 를 통해 해당 컴포넌트 호출시 나오는 return 값 설정
+class R003_ImportComponent extends Component{
+    render(){
+        return (
+            <h2>[this is importComponet]</h2>
+        )
+    }
+}
+
+export default R003_ImportComponent;
